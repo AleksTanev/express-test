374c4c7625493de7f90eb57649c3ab5eecfb5045
Aleksandar Tanev <91281906+AleksTanev@users.noreply.github.com>
2023-02-27 10:47:18 +0200

    added initial items
    Signed-off-by: Aleksandar Tanev <91281906+AleksTanev@users.noreply.github.com>

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello worllldddd");
+});
+
+app.post("/", (req, res) => {
+    res.send("Got a post request!");
 });
 
 app.listen(port, () => {
